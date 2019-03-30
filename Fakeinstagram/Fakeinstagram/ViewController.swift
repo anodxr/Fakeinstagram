66a88afa92205a2e7680382578e3ebcb5def792d
ruben <Ruben_Diplomado@MacBook-14.local>
2019-03-30 11:04:00 -0600

    add xcode project

---
+++ Fakeinstagram/Fakeinstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fakeinstagram
+//
+//  Created by Ruben Francisco Romero Aguado D3 on 3/30/19.
+//  Copyright © 2019 Ruben Francisco Romero Aguado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
